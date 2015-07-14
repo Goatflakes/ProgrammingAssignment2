7a6c30a7a62a4be97cd2e262cf915d012913ef4d
Cian Michael Moriarty <cian.moriarty@gmail.com>
2015-07-14 14:11:41 +1000

    Edited the comments to make them more sensible

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,20 @@
-## cachematrix.R: Implements a matrix which
+## cachematrix.R: Implements a matrix abstract data type which
 ##   1. is assumed to be invertible
 ##   2. computes its inverse when needed, and then caches it until
 ##      the matrix changes
 ## To create the matrix use makeCacheMatrix() optionally passing the
 ## intial matrix.
 ##
-## To get the inverse, use cacheSolve() passing the matrix.
-## The matrix must be intialised with an invertible matrix, either
-## via an argument to the makeCacheMatrix() call, or a later call
-## to myMatrix$set(), where myMatrix is the return value of
-## makeCacheMatrix()
+## To get the inverse, use cacheSolve() passing the return value of
+## makeCacheMatrix.
+##
+## The matrix must have been intialised with an invertible matrix,
+## either via an argument to the makeCacheMatrix() call, or a later
+## call to myMatrix$set(matrix), where myMatrix is the return value of
+## makeCacheMatrix() and matrix is an invertible matrix
 
-## Return a cached matrix, which is really a list, containing a funtion to:
+## Return a cached matrix, which is really a list, containing
+## functions to:
 ##   1. set the value of the matrix (NB: matrix must be invertible)
 ##   2. get the value of the matrix
 ##   3. set the value of the matrix inverse. For cacheSolve()
@@ -23,7 +26,9 @@ makeCacheMatrix <- function(x = matrix()) {
   
   # Each call to makeCacheMatrix makes a new environment and new
   # x and inv that can be accessed by the <<- operator in functions
-  # that are enclosed in makeCacheMatrix().
+  # that are enclosed in makeCacheMatrix(). These functions are called
+  # closures because they have an enclosing environment which is
+  # accessible to them.
 
   # This is because <<- searches its parent environment.
 
@@ -51,21 +56,16 @@ makeCacheMatrix <- function(x = matrix()) {
   setInv <- function(inverse) inv <<- inverse
 
   # getInv() is a function that returns the inverse inv
-  # this is stored in the parent environment so <<- is needed to
-  # access it.
-  
+
   # getInv() is used only by the internal functions. If you want the
   # get the inverse, call cacheSolve(myMatrix) instead, where
   # myMatrix is the return value of makeCacheMatrix()
   getInv <- function() inv
   
   # Return a list of all the accessor functions. Note that we don't
-  # need add inv or x to the list, because they are stored in the
-  # closure created by the makeCacheMatrix() call and are accessible
-  # the accessor functions with the <<- operator, which can set
-  # variables from parent (or grandparent, etc.) environments as it
-  # searches the entire list of parent environments to find the
-  # variable it is assigning.
+  # need add inv or x to the list, because the functions are closures
+  # which can see x and inv in this environment & set them with the
+  # <<- operator.
   list(set = set, get = get,
        setInv = setInv,
        getInv = getInv)
